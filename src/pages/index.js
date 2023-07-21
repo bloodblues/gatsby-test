b1497b19fd3ae3989d9430c538f1e1bb89581a96
Pedro Cubillos <bloodblues@gmail.com>
2023-07-21 14:22:27 -0400

    ultimas funciones

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,8 @@
 import * as React from 'react'
 import Layout from '../components/layout'
 import { StaticImage } from 'gatsby-plugin-image'
+import Seo from '../components/seo'
+
 
 const IndexPage = () => {
   return (
@@ -18,6 +20,6 @@ const IndexPage = () => {
   )
 }
 
-export const Head = () => <title>Home Page</title>
+export const Head = () => <Seo  title="Home Pageee" />
 
 export default IndexPage
